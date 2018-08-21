f99a3a634e8407bd886b42094963931210b2590b
Alvaro Garcia <derecerca@gmail.com>
2018-08-22 01:18:29 +0200

    Refactor: extract explaining constants

--- src/main/kotlin/starter/kotlin/SandwichScheduler.kt
+++ src/main/kotlin/starter/kotlin/SandwichScheduler.kt
@@ -1,16 +1,20 @@
 package starter.kotlin
 
 class SandwichScheduler(val amountOfSandwiches: Int) {
+
+    private val SANDWICH_PREPARATION_TIME = 60
+    private val SANDWICH_SERVING_TIME = 30
+
     fun calculate(): Schedule {
         val tasks = mutableListOf<Task>()
         tasks.add(Task("0", "start making sandwich 1"))
-        var time = 60
+        var time = SANDWICH_PREPARATION_TIME
         for (i in 1..amountOfSandwiches) {
             tasks.add(Task("" + time, "serve sandwich $i"))
-            time += 30
+            time += SANDWICH_SERVING_TIME
             if (i < amountOfSandwiches) {
                 tasks.add(Task("" + time, "make sandwich ${i + 1}"))
-                time += 60
+                time += SANDWICH_PREPARATION_TIME
             }
         }
         tasks.add(Task("" + time, "take a well earned break"))
