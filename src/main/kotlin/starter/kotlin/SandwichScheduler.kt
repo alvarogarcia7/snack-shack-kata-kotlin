73e110024520e65f177286c123ccca7bbbe17d9e
Alvaro Garcia <derecerca@gmail.com>
2018-08-22 01:17:36 +0200

    Refactor: remove duplication

--- src/main/kotlin/starter/kotlin/SandwichScheduler.kt
+++ src/main/kotlin/starter/kotlin/SandwichScheduler.kt
@@ -5,14 +5,14 @@ class SandwichScheduler(val amountOfSandwiches: Int) {
         val tasks = mutableListOf<Task>()
         tasks.add(Task("0", "start making sandwich 1"))
         var time = 60
-        for (i in 1..amountOfSandwiches - 1) {
+        for (i in 1..amountOfSandwiches) {
             tasks.add(Task("" + time, "serve sandwich $i"))
             time += 30
-            tasks.add(Task("" + time, "make sandwich ${i + 1}"))
-            time += 60
+            if (i < amountOfSandwiches) {
+                tasks.add(Task("" + time, "make sandwich ${i + 1}"))
+                time += 60
+            }
         }
-        tasks.add(Task("" + time, "serve sandwich $amountOfSandwiches"))
-        time += 30
         tasks.add(Task("" + time, "take a well earned break"))
         return Schedule(tasks)
     }
